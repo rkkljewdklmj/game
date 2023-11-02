9f4f2f6f63b9b9aa87a8886faf0a314f16047378
harry <146829307+rkkljewdklmj@users.noreply.github.com>
2023-11-02 15:39:50 +0000

    help me nigel

--- Assets/PlayerHealth.cs
+++ Assets/PlayerHealth.cs
@@ -5,37 +5,29 @@ using UnityEngine;
 public class PlayerHealth : MonoBehaviour
 {
     public int maxHealth = 100;
-    public int currentHealth;
+    public int health;
     public Animator animator;
     public HealthBar healthBar;
 
     void Start()
     {
-        currentHealth = maxHealth;
-        healthBar.SetMaxHealth(maxHealth);
+       health = maxHealth;
     }
+
+    public void TakeDamage(int damage)
+    {
+      health -= damage;
+      if(health <= 0)
+      {
+         Destroy(gameObject);
+      }
+    }
+   
+   
+
 
    
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            TakeDamage(20);
-        }
-    }
-
-
-    void TakeDamage(int damage)
-    {
-        currentHealth -= damage;
-
-        healthBar.SetHealth(currentHealth);
-
-        if (currentHealth <= 0)
-        {
-            Die();
-        }
-    }
+    
     void Die()
     {
         Debug.Log("you died");
